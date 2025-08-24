1360508a87d60b9d3cf89112e53475fc839c1204
Raj Pandit <rajpandit956@gmail.com>
2025-08-24 20:36:08 +0530

    Update Restaurant Management System.sql

--- Restaurant Management System.sql
+++ Restaurant Management System.sql
@@ -1,25 +1,39 @@
 use sqlps44;
 create table restaurants5(restaurant_id int primary key,restaurant_name varchar(100) not null,cuisine_type varchar(50),address varchar(255),phone varchar(20));
+
+
 select * from restaurants5;
+
 desc restaurants5;
+
 drop table restaurants;
+
 insert into restaurants5(restaurant_id,restaurant_name,cuisine_type,address,phone)
 value(1,'pasta palace','italian','123 Main St','5551234567'),
 (2,'taco time','mexican','456 Elm St','5559876543'),
      (3,'burger barn','american','789 oak St','5557891234');
+
+
 create table menutable5(item_id int primary key,restaurant_id int,item_name varchar(100) not null,description varchar(255),price decimal(10,2),
 foreign key(restaurant_id) references restaurants5(restaurant_id));
 desc menutable5;
+
 drop table menutable;
+
 select * from menutable5;
+
 insert into menutable5(item_id,restaurant_id,item_name,description,price)
 value(101,1,'spaghetti carnobora','pasta with eggs','12.99'),
 (102,1,'margherita pizza','classic pizza with tomato','14.99'),
 (103,2,'taco combo','assorted tacos with sides','9.99'),
 (104,2,'quesadilia','çheese-filled tortilia','8.99'),
 (105,3,'classic burger','beef patty with letture','10.99');
+
+
 create table orders5(order_id int primary key,item_id int,customer_name varchar(100) not null,delivery_address varchar(255),order_date date,total_amt decimal(10,2),
 foreign key (item_id) references menutable5(item_id));
+
+
 drop table orders;
 insert into orders5(order_id,item_id,customer_name,delivery_address,order_date,total_amt)
 values(201,101,'john doe','123 main st','2024-04-15','12.99'),
